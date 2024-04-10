199b394a9a24fc3770c4eeef726f85195a91c038
diandraf2003 <116277781+diandraf2003@users.noreply.github.com>
2024-04-10 21:33:18 +0300

    web

--- interfata proiect/index.php
+++ interfata proiect/index.php
@@ -16,6 +16,59 @@ if ($_SESSION['isLogged']) {
   <link rel="stylesheet" type="text/css" href="index.css">
 </head>
 <body>
- 
+
+    <div class="form-fields">
+      <!-- Aici vor fi câmpurile pentru descriere și formular -->
+      <input type="text" placeholder="Descriere" class="description-input">
+      <textarea placeholder="Introdu formularul aici" class="form-textarea"></textarea>
+      <button class="publica-button">Publică</button>
+    </div>
+
+  
+  <div class="container">
+    <div class="feedback-form">
+      <img src="profile.jpg" alt="Iconiță utilizator"> Utilizator anonim
+      <h2>Titlu formular</h2>
+      <p>Descriere formular</p>
+      <a href="#">Accesează formularul</a>
+  </div>
+  <div class="container">
+    <div class="feedback-form">
+      <img src="profile.jpg" alt="Iconiță utilizator"> Utilizator anonim
+      <h2>Titlu formular</h2>
+      <p>Descriere formular</p>
+      <a href="#">Accesează formularul</a>
+  </div>
+  <div class="container">
+    <div class="feedback-form">
+      <img src="profile.jpg" alt="Iconiță utilizator"> Utilizator anonim
+      <h2>Titlu formular</h2>
+      <p>Descriere formular</p>
+      <a href="#">Accesează formularul</a>
+  </div>
+  <div class="container">
+    <div class="feedback-form">
+      <img src="profile.jpg" alt="Iconiță utilizator"> Utilizator anonim
+      <h2>Titlu formular</h2>
+      <p>Descriere formular</p>
+      <a href="#">Accesează formularul</a>
+  </div>
+  <div class="container">
+    <div class="feedback-form">
+      <img src="profile.jpg" alt="Iconiță utilizator"> Utilizator anonim
+      <h2>Titlu formular</h2>
+      <p>Descriere formular</p>
+      <a href="#">Accesează formularul</a>
+  </div>
+  <div class="container">
+    <div class="feedback-form">
+      <img src="profile.jpg" alt="Iconiță utilizator"> Utilizator anonim
+      <h2>Titlu formular</h2>
+      <p>Descriere formular</p>
+      <a href="#">Accesează formularul</a>
+  </div>
+</div>  
+
+
 </body>
 </html>
